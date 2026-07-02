c3b67536733cda2f2496ab4687e845e6e8e06593
Sambuddha Saha <sanghamitrabasu007@gmail.com>
2026-07-02 11:08:56 +0530

    Add files via upload

---
+++ 03_Loop/Reverse.c
@@ -0,0 +1,21 @@
+ /*
+Program: Reversing a number 
+Author: Sambuddha
+Description:
+User enters a number and the program reverses it .
+*/
+ #include <stdio.h>
+int main (void)
+{
+int  temp, reverse=0,digit;
+printf("Enter the number you want to reverse :");
+scanf("%d",&temp);
+while(temp>0)
+{
+    digit=temp%10;  
+    reverse=reverse*10+digit;
+   temp=temp/10;  
+}
+ printf("%d",reverse);
+return 0;
+}
